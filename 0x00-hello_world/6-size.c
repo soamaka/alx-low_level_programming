d7188a72f2a2fbf437f35cceb28016cabfc3137c
sophia <soamaka@gmail.com>
2023-02-16 08:01:50 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - A program that prints the size of various types
- * Return 0
+ * Return: 0
  */
 int main(void)
 {
@@ -11,10 +11,10 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("Size of a char: %ld byte\n",sizeof(a));
-printf("Size of an int: %ld byte\n",sizeof(b));
-printf("Size of a long int: %ld byte\n",sizeof(c));
-printf("Size of a long long int: %ld byte \n",sizeof(d));
-printf("Size of a float: %ld byte \n",sizeof(f));
-return (0)
+printf("Size of a char: %ld byte\n", sizeof(a));
+printf("Size of an int: %ld byte\n", sizeof(b));
+printf("Size of a long int: %ld byte\n", sizeof(c));
+printf("Size of a long long int: %ld byte \n", sizeof(d));
+printf("Size of a float: %ld byte \n", sizeof(f));
+return (0);
 }
