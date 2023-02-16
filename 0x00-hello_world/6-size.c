54604d8b9da705722d091170f6f3aae1bf8c7c69
sophia <soamaka@gmail.com>
2023-02-16 08:04:46 -0800

    new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("Size of a char: %ld byte\n", sizeof(a));
-printf("Size of an int: %ld byte\n", sizeof(b));
-printf("Size of a long int: %ld byte\n", sizeof(c));
-printf("Size of a long long int: %ld byte \n", sizeof(d));
-printf("Size of a float: %ld byte \n", sizeof(f));
+printf("Size of a char: %ld byte(s)\n", sizeof(a));
+printf("Size of an int: %ld byte(s)\n", sizeof(b));
+printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
+printf("Size of a float: %ld byte(s)\n", sizeof(f));
 return (0);
 }
