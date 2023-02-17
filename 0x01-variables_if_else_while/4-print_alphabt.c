ca9ae162b32d7efcf266d1ab3cb6ba0cfaf80d9d
sophia <soamaka@gmail.com>
2023-02-17 06:15:15 -0800

    p

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= '2') {
+			if ((c != 'q' && c != 'e') && c <= '2')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
