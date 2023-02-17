64c4bfde2cdb5fd2bd4e25d889405cffc6e3fd44
sophia <soamaka@gmail.com>
2023-02-16 17:38:15 -0800

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * main - A c program that prints a line to the standard error
- * Return : 1(success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+exit(1);
 }
