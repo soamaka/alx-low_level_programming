336e1f88f9f891f5b961044ec4a648726dbdb938
sophia <soamaka@gmail.com>
2023-02-21 17:50:38 -0800

    fifty

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,36 +1,25 @@
 #include <stdio.h>
-/**main - a program that prints the first 50 Fibonacci numbers
+/**
+ * main - a program that prints the first 50 Fibonacci numbers
  * Return: 0
  */
 int main(void)
 {
-	int count = 3;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	long int first = 1, second = 2;
-	long int next = first + second;
-
-	printf("%lu, ", first);
-	printf("%lu, ", second);
-
-	while (count <= 50)
+	for (count = 0, count < 50; count++)
 	{
-		/*Let's check if we are at the end of the list, if we are close with a new line */
-		if (count == 50)
-		{
-			printf("%lu \n", next);
-		}
-		else  /*if we are not at the end of the list, add a comma after the number */
-		{
-			printf("%lu, ", next);
-		}
+		sum = fib1 + fib2;
+		printf("%lu", sum");
 
-		/*Reset the variables to get the next number*/
-		first = second;
-		second = next;
+		fib1 = fib2;
+		fib2 = sum;
 
-		/* after resetting the variables, you need to find the next number */
-		next = first + second;
-		count++;
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
 	}
 
 	return (0);
