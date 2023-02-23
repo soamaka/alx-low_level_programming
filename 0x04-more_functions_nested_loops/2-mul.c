f7fd39a1c3911947cd85e8092abc21622b8cee23
sophia <soamaka@gmail.com>
2023-02-23 06:33:47 -0800

    multipy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - a function that multiplies two integers
+ * @a: The first number to multiply
+ * @b: The second number to multiply
+ * Return: The result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
