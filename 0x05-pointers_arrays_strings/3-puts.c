6089191bae34aa99981cbf8c70a148efed86644e
sophia <soamaka@gmail.com>
2023-02-27 01:33:43 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout
+ * @str: constant character
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
