0b279237eb77142d032396f7217309a169ee3d85
sophia <soamaka@gmail.com>
2023-03-23 08:37:47 -0700

    last task

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct format - data type of format
+ * @op: format
+ * @f: function
+ */
+
+typedef struct format
+{
+	char *op;
+	void(*f)(va_list all);
+} f;
+
 #endif
