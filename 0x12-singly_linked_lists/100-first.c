ac6eb1e3a6d336d9ff61d73632fdb38b17e91c1d
sophia <soamaka@gmail.com>
2023-03-30 07:20:25 -0700

    new

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * printString - prints a string before the main function is executed
+ */
+
+void printString(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
